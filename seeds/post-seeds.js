02b7dc3d74e503363b86b892572417d513aade1d
Marcio R <Murcielago203@gmail.com>
2022-03-01 21:48:19 -0500

    updated seeds and models

---
+++ seeds/post-seeds.js
@@ -0,0 +1,38 @@
+const { Post } = require('../models');
+
+const postdata = [
+  {
+    title: 'Donec posuere metus vitae ipsum.',
+    description: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
+    user_id: 10
+  },
+  {
+    title: 'Morbi non quam nec dui luctus rutrum.',
+    description: 'https://nasa.gov/donec.json',
+    user_id: 8
+  },
+  {
+    title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
+    description: 'https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
+    user_id: 1
+  },
+  {
+    title: 'Nunc purus.',
+    description: 'http://desdev.cn/enim/blandit/mi.jpg',
+    user_id: 4
+  },
+  {
+    title: 'Pellentesque eget nunc.',
+    description: 'http://google.ca/nam/nulla/integer.aspx',
+    user_id: 7
+  },
+  {
+    title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    description: 'https://stanford.edu/consequat.png',
+    user_id: 4
+  }
+];
+
+const seedPosts = () => Post.bulkCreate(postdata);
+
+module.exports = seedPosts;
